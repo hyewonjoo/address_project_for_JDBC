3612ddafe6bf086eb2d941054361d64c2298906b
hyewonJoo <run98@naver.com>
2022-05-12 01:50:24 +0900

    address program

---
+++ Main.java
@@ -0,0 +1,23 @@
+package com.test.java.mvc;
+
+public class Main {
+	public static void main(String[] args) {
+		// 메인 역활 > Service관리
+
+		Service service = new Service();
+
+		service.start();
+
+
+
+		/*
+		- MVC Pattern
+		- Model > Data > Address.java (+ DAO.java)
+		- View > 출력 > View.java
+		- Controller > 운용 역할. 제어 > Main.java, Service.java
+		 */
+
+	}
+}
+
+
